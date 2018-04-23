57f5e8b9f54702a787bead31239640d9832ea212
Aaron-Zhao123 <aaronzhao0731@gmail.com>
2018-04-23 19:06:42 +0100

    change scale to macs/s, need to rerun expriments on 1 single gpu

--- benchmark_plots.py
+++ benchmark_plots.py
@@ -4,12 +4,23 @@ import matplotlib
 matplotlib.use
 import matplotlib.pyplot as plt
 import numpy as np
+from matplotlib.backends.backend_pdf import PdfPages
 
 
 plt.style.use('ggplot')
 
 
 def main():
+    configs = {
+        'batch': 256,
+        'img-size': 32,
+        'in-channels': 128,
+        'out-channels': 128,
+        'kernel-size': 3,
+        'stride': 1,
+        'dilation': 1,
+        'groups': 1,
+    }
     x_axis = {
         'in-channels': [16, 32, 64, 128, 256, 512, 1024, 2048],
         'out-channels': [16, 32, 64, 128, 256, 512, 1024, 2048],
@@ -26,31 +37,51 @@ def main():
     keys = x_axis.keys()
     first_plot_keys = ['in-channels', 'out-channels', 'kernel-size']
     second_plot_keys = ['stride', 'dilation', 'groups']
+    pp = PdfPages('plot1.pdf')
     plt.figure(1)
     for i, key in enumerate(first_plot_keys):
         plt.subplot(*[len(first_plot_keys), 1, i+1])
         y_key = '--' + key
         lines = []
-        for j, label in enumerate(components):
-            line, = plt.plot(
-                x_axis[key], y_axis[y_key][j, :], '-o', label=label)
-            lines.append(line)
+        gpu_times = y_axis[y_key][1, :]
+        macs = [compute_macs(configs, (key, time)) for time in gpu_times]
+        line, = plt.plot(
+            x_axis[key], macs, '-o', label='gpu')
+        lines.append(line)
         plt.legend(handles=lines)
         plt.xlabel(key)
-        plt.ylabel('time')
+        plt.ylabel('time, macs/s')
+        plt.tight_layout()
+    pp.savefig()
+    pp.close()
+
+    pp = PdfPages('plot2.pdf')
     plt.figure(2)
     for i, key in enumerate(second_plot_keys):
         plt.subplot(*[len(second_plot_keys), 1, i+1])
         y_key = '--' + key
         lines = []
-        for j, label in enumerate(components):
-            line, = plt.plot(
-                x_axis[key], y_axis[y_key][j, :], '-o', label=label)
-            lines.append(line)
+        gpu_times = y_axis[y_key][1, :]
+        macs = [compute_macs(configs, (key, time)) for time in gpu_times]
+        line, = plt.plot(
+            x_axis[key], macs, '-o', label='gpu')
+        lines.append(line)
         plt.legend(handles=lines)
         plt.xlabel(key)
-        plt.ylabel('time')
-    plt.show()
+        plt.ylabel('time, macs/s')
+    pp.savefig()
+    pp.close()
+
+
+def compute_macs(configs, key_value_tuple=None):
+    if key_value_tuple:
+        key, value = key_value_tuple
+        configs[key] = value
+    c = configs
+    macs = c['batch'] * c['in-channels'] * c['out-channels']
+    macs *= (float(c['img-size']) ** 2) * (float(c['kernel-size']) ** 2)
+    macs *= (1 / float(c['groups'])) * ((1 / float(c['stride']) ** 2))
+    return macs
 
 
 if __name__ == "__main__":
--- loop_sim.py
+++ loop_sim.py
@@ -1,7 +1,12 @@
 import argparse
 import math
 from functools import reduce
+import matplotlib
+matplotlib.use
+import matplotlib.pyplot as plt
+import numpy as np
 
+plt.style.use('ggplot')
 
 '''
 Example conv loop
@@ -16,7 +21,7 @@ output_fm[fo][row][col]+=weights[fo][fi][i][j]*in_fm[fi][stride*row+i][stride*co
 
 
 class PE(object):
-    def __init__(self, width=10, depth=10):
+    def __init__(self, width=100, depth=10):
         '''
         Args:
             width: The number of parallel PEs.
@@ -43,7 +48,7 @@ class PE(object):
         process all parallel input
         '''
         self.factor = math.ceil(wp / float(self.width)) / float(self.depth)
-        throughput = 1 / float(self.factor)
+        throughput = 1 / float(self.factor) * wp
         return (self.factor * ni, throughput)
 
     def memory(self, wp, target_index):
@@ -74,34 +79,105 @@ class PE(object):
         return (write_tp, read_tp, onchip_size)
 
 
-def main(args):
+class MetaData(object):
+    def __init__(self):
+        self.names = ['wr_tps', 'rd_tps', 'compute_tps', 'latencies', 'sizes']
+        self.unroll_names = [
+            'OutRow', 'OutCol', 'OutChannels', 'InChannels', 'Kernel']
+        self.wr_tps = []
+        self.rd_tps = []
+        self.compute_tps = []
+        self.latencies = []
+        self.sizes = []
+
+    def add(self, tps, latency, size):
+        self.wr_tps.append(tps[0])
+        self.rd_tps.append(tps[1])
+        self.compute_tps.append(tps[2])
+        self.latencies.append(latency)
+        self.sizes.append(size)
+
+    def generate_meta(self):
+        return np.array(
+            [self.wr_tps, self.rd_tps, self.compute_tps, self.latencies,
+             self.sizes])
+
+
+def workload_pe(out_img=32, out_channels=256, in_channels=256, kernel_size=3):
     # row, col, fo, fi, i, j
     iterator_bounds = [
-        args.out_img, args.out_img, args.out_channels, args.in_channels,
-        args.kernel_size, args.kernel_size]
+        out_img, out_img, out_channels, in_channels, kernel_size,
+        kernel_size]
     print('The are {} iterators with the following bounds: {}'.format(
         len(iterator_bounds), iterator_bounds
     ))
     pe = PE()
     total_iters = reduce(lambda x, y: x*y, iterator_bounds)
-    for i in range(len(iterator_bounds)):
+    meta_data = MetaData()
+    # ignore one kernel size dim
+    for i in range(len(iterator_bounds) - 1):
         unrolling = iterator_bounds[i]
         iters = total_iters / unrolling
         pe.compute(unrolling, iters, i)
-        import pdb; pdb.set_trace()
+        tps = [pe.wr_tp, pe.rd_tp, pe.compute_tp]
+        print('unroll factor: ', unrolling)
+        print('wr, rd and compute throughputs : {}'.format(tps))
+        print('on-chip size {}, latency {}'.format(pe.size, pe.latency))
+        meta_data.add(tps, pe.size, pe.latency)
+    return meta_data.generate_meta()
+
+
+def main():
+    configs = {
+        'out_img': 32,
+        'out_channels': 128,
+        'in_channels': 128,
+        'kernel_size': 3,
+    }
+    # test out_imgs
+    test_range = [2, 4, 8, 16, 32, 128, 256, 512]
+    metas = []
+    for value in test_range:
+        configs['out_img'] = value
+        meta_data = workload_pe(**configs)
+        metas.append(meta_data)
+    # restructure
+    plot(test_range, 'img_sizes', metas, 1)
+    import pdb; pdb.set_trace()
+
+
+def plot(x_axis, x_axis_label, metas, figure_id=1):
+    plt.figure(1)
+    names = ['wt_tp', 'rd_tp', 'compute_tp', 'on-chip size', 'latency']
+
+    plt.figure(figure_id)
+    # plot throughputs
+    plt.subplot(311)
+    lines = []
+    for j, y_axis in enumerate([wt_tps, rd_tps, compute_tps]):
+        line, = plt.plot(
+            x_axis, y_axis, '-o', label=names[j])
+        lines.append(line)
+    plt.legend(handles=lines)
+    plt.xlabel(x_axis_label)
+    plt.ylabel('throughputs')
+
+    plt.subplot(312)
+    line, = plt.plot(
+        x_axis, sizes, '-o', label=names[3])
+    plt.legend(handles=[line])
+    plt.xlabel(x_axis_label)
+    plt.ylabel('on-chip elements')
+
+    plt.subplot(313)
+    line, = plt.plot(
+        x_axis, latencies, '-o', label=names[4])
+    plt.legend(handles=[line])
+    plt.xlabel(x_axis_label)
+    plt.ylabel('on-chip elements')
+
+    plt.show()
 
 
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser()
-    parser.add_argument("--batch", type=int, default=256)
-    # this is the input row and col size
-    parser.add_argument("--in-img", type=int, default=32)
-    parser.add_argument("--out-img", type=int, default=32)
-    parser.add_argument("--in-channels", type=int, default=128)
-    parser.add_argument("--out-channels", type=int, default=128)
-    parser.add_argument("--kernel-size", type=int, default=3)
-    parser.add_argument("--stride", type=int, default=1)
-
-    args = parser.parse_args()
-
-    main(args)
+    main()
