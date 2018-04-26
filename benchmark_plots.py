0d225ff44c4f7989118d46e02e6f15810d4c83b7
Aaron-Zhao123 <aaronzhao0731@gmail.com>
2018-04-26 16:37:08 +0100

    fixed nano second and TFLOPs, still an order of 10 is missing

--- benchmark_plots.py
+++ benchmark_plots.py
@@ -5,6 +5,8 @@ matplotlib.use
 import matplotlib.pyplot as plt
 import numpy as np
 from matplotlib.backends.backend_pdf import PdfPages
+import copy
+
 
 
 plt.style.use('ggplot')
@@ -22,65 +24,79 @@ def main():
         'groups': 1,
     }
     x_axis = {
-        'in-channels': [16, 32, 64, 128, 256, 512, 1024, 2048],
-        'out-channels': [16, 32, 64, 128, 256, 512, 1024, 2048],
+        'in-channels': [16, 32, 64, 128, 256, 512, 800, 1024, 2048, 4096],
+        'out-channels': [16, 32, 64, 128, 256, 512, 800, 1024, 2048, 4096],
         'kernel-size': [1, 2, 3, 4, 5, 6, 7, 8, 16, 32],
+        'img-size': [4, 8, 16, 32, 64, 128],
         'stride': [1, 2, 3, 4, 5, 6],
         'dilation': [1, 2, 3, 4],
         'groups': [1, 2, 4, 8, 16, 32, 64, 128],
     }
+    # theoretical flops
+    TITAN_1080 = 10.6 * (10 ** 12)
+    TITAN_1080 = TITAN_1080 / float(2)
     with open('profile.pkl', 'rb') as f:
         y_axis = pickle.load(f)
     for key, item in y_axis.items():
-        y_axis[key] = np.array(item).T / 100
+        y_axis[key] = np.array(item).T
     components = ['cpu_time', 'gpu_time']
     keys = x_axis.keys()
-    first_plot_keys = ['in-channels', 'out-channels', 'kernel-size']
+    first_plot_keys = ['in-channels', 'out-channels', 'kernel-size', 'img-size']
     second_plot_keys = ['stride', 'dilation', 'groups']
     pp = PdfPages('plot1.pdf')
-    plt.figure(1)
+    plt.figure(1, figsize=(10, 10))
     for i, key in enumerate(first_plot_keys):
         plt.subplot(*[len(first_plot_keys), 1, i+1])
         y_key = '--' + key
         lines = []
         gpu_times = y_axis[y_key][1, :]
-        macs = [compute_macs(configs, (key, time)) for time in gpu_times]
+        print(gpu_times)
+        macs = [compute_macs(configs, (key, value)) for value in x_axis[key]]
+        macs_per_second = np.array(macs) / (np.array(gpu_times) * 1e-9)
         line, = plt.plot(
-            x_axis[key], macs, '-o', label='gpu')
+            x_axis[key], macs_per_second, '-o', label='gpu')
+        lines.append(line)
+        line, = plt.plot(
+            x_axis[key],
+            len(x_axis[key]) * [TITAN_1080], '-', label='theoretical')
         lines.append(line)
         plt.legend(handles=lines)
         plt.xlabel(key)
-        plt.ylabel('time, macs/s')
+        plt.ylabel('Tp (macs/s)')
         plt.tight_layout()
     pp.savefig()
     pp.close()
 
     pp = PdfPages('plot2.pdf')
-    plt.figure(2)
+    plt.figure(2, figsize=(10, 10))
     for i, key in enumerate(second_plot_keys):
         plt.subplot(*[len(second_plot_keys), 1, i+1])
         y_key = '--' + key
         lines = []
+        print(gpu_times)
         gpu_times = y_axis[y_key][1, :]
-        macs = [compute_macs(configs, (key, time)) for time in gpu_times]
+        macs = [compute_macs(configs, (key, value)) for value in x_axis[key]]
+        macs_per_second = np.array(macs) / (np.array(gpu_times) * 1e-9)
         line, = plt.plot(
-            x_axis[key], macs, '-o', label='gpu')
+            x_axis[key], macs_per_second, '-o', label='gpu')
         lines.append(line)
         plt.legend(handles=lines)
         plt.xlabel(key)
-        plt.ylabel('time, macs/s')
+        plt.ylabel('Tp (macs/s)')
+        plt.tight_layout()
     pp.savefig()
     pp.close()
+    import pdb; pdb.set_trace()
 
 
 def compute_macs(configs, key_value_tuple=None):
+    c = copy.deepcopy(configs)
     if key_value_tuple:
         key, value = key_value_tuple
-        configs[key] = value
-    c = configs
+        c[key] = value
     macs = c['batch'] * c['in-channels'] * c['out-channels']
     macs *= (float(c['img-size']) ** 2) * (float(c['kernel-size']) ** 2)
-    macs *= (1 / float(c['groups'])) * ((1 / float(c['stride']) ** 2))
+    macs *= (1 / float(c['groups'])) * ((1 / (float(c['stride']) ** 2)))
     return macs
 
 
